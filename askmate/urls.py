2e4bd16f1c72360b4775d0dbd38193b36a8ab4ad
Anom Maharjan <anmmzn1@gmail.com>
2019-12-11 08:02:46 +0545

    created askmate  project

--- askmate/urls.py
+++ askmate/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('qna_app/', include('qna_app.urls')),
+    path('user_app/', include('user_app.urls'))
+
 ]
