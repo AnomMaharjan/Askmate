cf911518f190d6c58428023c5f2f122ea2a77176
Anom Maharjan <anmmzn1@gmail.com>
2019-12-11 08:16:38 +0545

    urls added to user and qna app

--- qna_app/urls.py
+++ qna_app/urls.py
@@ -17,8 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('qna/', include('qna_app.urls')),
-    path('user/', include('user_app.urls'))
+
 
 ]
