5be560eae3f7bad95bcf10761ca8416e21f43a9a
Anom Maharjan <anmmzn1@gmail.com>
2019-12-11 08:52:11 +0545

    models created for answer and question

--- qna_app/models.py
+++ qna_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class QuestionModel(models.Model):
+    title = models.CharField(max_length=255)
+    posted_by = models.CharField(max_length=120)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    question_desc= models.TextField()
+    question_img = models.ImageField(upload_to="QuestionImg")
+
+
+class AnswerModel(models.Model):
+    posted_by=models.CharField(max_length=120)
+    timestamp= models.DateTimeField(auto_now_add=True)
+    answer_desc = models.TextField()
+    answer_img = models.ImageField(upload_to="AnswerImg")
+    is_accepted= models.BooleanField()
+    votes= models.IntegerField()
+    question = models.ForeignKey(QuestionModel,on_delete=models.CASCADE) #answer gets deleted if question is deleted 
+
+
+
+
