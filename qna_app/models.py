45091af56a3643dc381a9cc5a77b71003d3d3b60
Anom Maharjan <anmmzn1@gmail.com>
2019-12-11 19:23:04 +0545

    added CategoryModel and also added foreign key in the QuestionModel

--- qna_app/models.py
+++ qna_app/models.py
@@ -8,9 +8,10 @@ class QuestionModel(models.Model):
     posted_by = models.CharField(max_length=120)
     timestamp = models.DateTimeField(auto_now_add=True)
     question_desc = models.TextField()
-    question_img = models.ImageField(upload_to="QuestionImg",blank=True, null=True)
-    question_votes= models.IntegerField(default=0)
-    #objects=object.manager
+    question_img = models.ImageField(
+        upload_to="QuestionImg", blank=True, null=True)
+    question_votes = models.IntegerField(default=0)
+    category = models.ForeignKey(CategoryModel, on_delete=models.CASCADE)
 
     def __str__(self):
         return(self.question_desc)
@@ -20,8 +21,17 @@ class AnswerModel(models.Model):
     posted_by = models.CharField(max_length=120)
     timestamp = models.DateTimeField(auto_now_add=True)
     answer_desc = models.TextField()
-    answer_img = models.ImageField(upload_to="AnswerImg",blank=True, null=True)
+    answer_img = models.ImageField(
+        upload_to="AnswerImg", blank=True, null=True)
     is_accepted = models.BooleanField(default=0)
     ans_votes = models.IntegerField(default=0)
     # answer gets deleted if question is deleted
     question = models.ForeignKey(QuestionModel, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return(self.answer_desc)
+
+
+class CategoryModel(models.Model):
+    category_title = models.CharField(max_length=255)
+    category_description = models.TextField()
