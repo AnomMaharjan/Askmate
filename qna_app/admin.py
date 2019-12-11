ec8478b46771b28da808f94eaff141bd8127f969
Anom Maharjan <anmmzn1@gmail.com>
2019-12-11 09:55:29 +0545

    modelschanged

--- qna_app/admin.py
+++ qna_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import QuestionModel, AnswerModel
 
-# Register your models here.
+admin.site.register(QuestionModel)
+admin.site.register(AnswerModel)
